062fa3dbfe9d033b38affa64cd111c6b50cd0ac1
Eduardo-Ozika <eduardoozika.ti@gmail.com>
2022-09-14 23:29:03 -0300

    getter and setter

--- src/main/java/com/projeto/projetoCadPessoas/model/entity/PessoaJuridica.java
+++ src/main/java/com/projeto/projetoCadPessoas/model/entity/PessoaJuridica.java
@@ -14,5 +14,13 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "tb_pessoajuridica")
 public class PessoaJuridica extends Pessoa{
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
     private String cnpj;
 }
